d6214cbdd8f54f2dfe1d52b72dd2d12a0e2007b9
Sasa Mora Roca <100560658+Sabata79@users.noreply.github.com>
2022-10-24 14:55:23 +0300

    Navbar raakile ,responsiivisuus toimii

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,22 +4,17 @@ import './navbar.css'
 export default function Navbar() {
 
   return (
-    
-<nav id="navbar" class="navbar navbar-expand-lg">
-  <div class="container-fluid">
-    <a id="header" class="navbar-brand">
-      <h1>FIUKE</h1>
-      <p>[Pyöräilijän kauppa]</p>
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item"><a class="nav-link" href="#pageOne">Pyörät 1</a></li>
-        <li class="nav-item"><a class="nav-link" href="#pageTwo">Pyörät 2</a></li>
-        <li class="nav-item"><a class="nav-link" href="#pageThree">Pyörät 3</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+    <nav id="navbar" className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <a id="header" className="navbar-brand">
+          <h1>FIUKE</h1>
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span className="navbar-toggler-icon"></span> </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          tähän tulee sitten komponentista droppeja
+          {/*//tähä väliin lisätään select komponenttejä*/}
+        </div>
+      </div>
+    </nav>
   )
 }
