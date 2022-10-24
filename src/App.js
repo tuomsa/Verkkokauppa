a02be604853bb9843a8fb2761e490ea8043da194
Sasa Mora Roca <100560658+Sabata79@users.noreply.github.com>
2022-10-24 13:01:46 +0300

    Navbar linkattu app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Navbar from './components/navbar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-   <h1>Add verkkokauppa here</h1>
+    <div className="container">
+      <Navbar/>
     </div>
   );
 }
