a95df1d5cd08c252eec03ad3a4db634254826b70
Sasa Mora Roca <100560658+Sabata79@users.noreply.github.com>
2022-10-24 18:07:13 +0300

    Fonttiveistelyä navbaariin

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,11 +8,11 @@ export default function Navbar() {
     <nav id="navbar" className="navbar navbar-expand-lg">
       <div className="container-fluid">
         <a id="header" className="navbar-brand">
-          <h1>FIUKE</h1>
+          <h1 className="logo">FIUKE</h1>
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span className="navbar-toggler-icon"></span> </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          tähän tulee sitten komponentista droppeja
+          Tähän tulee linkkejä
           {/*//tähä väliin lisätään select komponenttejä*/}
         </div>
       </div>
