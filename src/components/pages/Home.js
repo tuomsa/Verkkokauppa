86d4b4adc271c6d4a4cbd2c40efe82c95a6d73e4
Sasa Mora Roca <100560658+Sabata79@users.noreply.github.com>
2022-11-18 09:50:44 +0200

    home.css pikaparannus

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,8 @@
+import '../styles/home.css'
+
 export default function Home() {
   return (
-    <div>
+    <div className='content'>
       <h3>This is home page</h3>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem itaque dolorum nobis nihil porro. Reprehenderit, nostrum inventore molestiae explicabo sed soluta, amet hic quisquam blanditiis cumque corrupti omnis, dicta nam.</p>
     </div>
