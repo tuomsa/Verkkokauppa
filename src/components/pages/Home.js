3ea708fa92f7ca8e72e911c56cbcd0c449449dff
Sasa Mora Roca <100560658+Sabata79@users.noreply.github.com>
2022-11-21 20:36:41 +0200

    landinpage update

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,14 @@
 import '../styles/home.css'
+import Bgvideo from '../pages/bgvideo'
 
 export default function Home() {
   return (
-    <div className='content'>
-      <h3>This is home page</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem itaque dolorum nobis nihil porro. Reprehenderit, nostrum inventore molestiae explicabo sed soluta, amet hic quisquam blanditiis cumque corrupti omnis, dicta nam.</p>
-    </div>
+    <body>
+      <Bgvideo/>
+      <div className='content'>
+        <h3>This is home page</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem itaque dolorum nobis nihil porro. Reprehenderit, nostrum inventore molestiae explicabo sed soluta, amet hic quisquam blanditiis cumque corrupti omnis, dicta nam.</p>
+      </div>
+    </body>
   )
 }
