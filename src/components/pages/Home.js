5a7135eeec2284a5d9bca20ceb8948af8d92937d
Sasa Mora Roca <100560658+Sabata79@users.noreply.github.com>
2022-12-08 19:15:28 +0200

    fixed homescreen. Admin rdy

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,8 +4,11 @@ import Bgvideo from '../pages/bgvideo'
 export default function Home() {
   return (     
       <div className='content'>
+        <Bgvideo/>
+        <div className='textcontainer'>
         <h1> Fiuke - Pyörä juuri sinulle</h1>
         <p id="homeFront">Laadukas polkupyörien nettikauppa</p>
+      </div>
       </div>
   )
 }
