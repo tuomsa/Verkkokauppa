a9146ae191a75f8bbfdecb5aeb70af012795fb3e
Smeagolio <mika.lampinen344@gmail.com>
2022-12-16 16:40:50 +0200

    cart css

--- src/components/pages/cart.js
+++ src/components/pages/cart.js
@@ -8,46 +8,48 @@ export default function Cart(props) {
   return (
     <>
 
-    <div className={("cart-Items", "main")}>
+    <div className={("main")}>
+        <div className='cart-items'>
         <div className="cart-items-header">Cart Items</div>
  
-        {props.cartItems.length === 0 && ( <div className="cartItems-empty">Ostoskori on tyhjä.</div>
+        {props.cartItems.length === 0 && ( <div className="cartitems-empty">Ostoskori on tyhjä.</div>
         )} 
         <div>
             {props.cartItems.map((item) => (
                 <div key={item.tuotenro} className="cartitems-list">
-                    <img className='cartitems-image'
-                    src={item.kuva}  // imaget sql.llästä?
+                  {/*  <img className='cartitems-image' width="150px" height="auto" 
+                    //src={item.kuva}  // imaget sql.llästä? 
                     alt={item.tuotemalli}
-                    />
+                    /> */}
                     <div className="cartItems-name">{item.tuotemalli}</div>
                     <div className="cartItems-function">
                          <button
-                            className="cartItems-add"
+                            className="cartitems-add"
                             onClick={() => props.addToCart(item)}
                         >+</button> 
                         <button
-                            className="cartItems-substract"
+                            className="cartitems-substract"
                             onClick={() => props.subtractFromCart(item)}
                         >-</button> 
                          <button
-                            className="cartItems-remove"
+                            className="cartitems-remove"
                            onClick={() => props.removeFromCart(item)}
                         >Remove</button> 
                         </div>
-                        <div className="cartItems-price">
+                        <div className="cartitems-price">
                             {item.amount} x {item.hinta}€
                         </div>
 
                     </div>
             ))}
         </div>
-        <div className="cartItems-totalprice-name">
+        <div className="cartitems-totalprice-name">
                         Kokonaishinta
-            <div className="cartItems-total-price">
+            <div className="cartitems-total-price">
                 ${totalPrice}
             </div>
         </div>
+    </div>
     </div>
     </>
   )
